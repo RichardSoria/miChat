4cc5bae313a436cdd7fe2adcb75eece444e33429
PaulCabreraEPN <paul.cabrera@epn.edu.ec>
2025-05-07 15:39:42 -0500

    Chat Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { ChatService, Message } from '../services/chat.service';
+import { NgIf, NgFor, CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonicModule, CommonModule, FormsModule, NgFor
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  messages: Message[] = [];
+  newMessage: string = '';
+  sender: string = 'User';
+  userName: string = 'Richard Mauricio Soria Asanza';
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.chatService.getMessages().subscribe(
+      res => {
+        this.messages = res;
+      }
+    )
+  }
+  sendMessages() {
+    if (this.newMessage.trim() !== '') {
+      this.chatService.sendMessages(this.newMessage, this.userName).then(() => {
+        this.newMessage = '';
+      });
+    }
+  }
 }
+
+
